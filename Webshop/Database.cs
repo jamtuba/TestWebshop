067496e4db3519457a7a31a0b49a773af7b187dc
Janus Mogensen <38835602+jamtuba@users.noreply.github.com>
2021-11-13 17:05:28 +0100

    Initial commit

---
+++ Webshop/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop
+{ 
+    /// <summary>
+    /// Database-klasse. Alle metoder i denne klasse kaster en exception, fordi du ikke har adgang til databasen i test.
+    /// Du skal ikke rette noget i denne klasse.
+    /// 
+    /// </summary>
+    public class Database
+    {
+        public void AddToBasket(string brugernavn, int varenummer, int antal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromBasket(string brugernavn, int varenummer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BasketEntry[] GetBasket(string brugernavn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Article GetArticle(int varenummer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Webshop/ShoppingBasket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop
+{
+    public class ShoppingBasket
+    {
+        /// <summary>
+        /// Tilføjer en vare til indkøbskurven
+        /// </summary>
+        /// <param name="brugernavn">brugerens id</param>
+        /// <param name="varenummer">varenummer på den vare der skal tilføjet</param>
+        /// <param name="antal">antallet af varer</param>
+        /// <returns>Det samlede antal varer i kurven</returns>
+        public int AddToBasket(string brugernavn, int varenummer, int antal)
+        {
+            Database db = new();
+            if (antal > 0)
+            {
+                db.AddToBasket(brugernavn, varenummer, antal);
+            } else if (antal == 0)
+            {
+                db.RemoveFromBasket(brugernavn, varenummer);
+            }
+            
+            BasketEntry[] varer = db.GetBasket(brugernavn);
+            int totalAntal = 0;
+            foreach (BasketEntry vare in varer) {
+                totalAntal = totalAntal + vare.Antal;
+            }
+            return totalAntal;
+        }
+
+        /// <summary>
+        /// Returnerer den samlede pris for alle varer i indkøbskurven
+        /// 
+        /// </summary>
+        /// <param name="brugernavn">brugerens id</param>
+        /// <returns>Den samlede pris som et beløb i kr.</returns>
+        public double GetTotalPrice(string brugernavn)
+        {
+            Database db = new();
+            BasketEntry[] varer = db.GetBasket(brugernavn);
+            int totalPris = 0;
+            foreach (BasketEntry entry in varer)
+            {
+                Article vare = db.GetArticle(entry.Varenummer);
+                totalPris = totalPris + entry.Antal * (int)vare.Price;
+            }
+            return totalPris;
+
+        }
+    }
+}
